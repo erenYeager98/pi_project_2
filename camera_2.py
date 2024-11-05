7aab1e6d91cd115afaa64dc18ac6026a44c41c35
DineshKumar <111277376+erenYeager98@users.noreply.github.com>
2024-11-05 13:13:31 +0530

    Add Function: Shutdown by Pin Trigger

--- camera_2.py
+++ camera_2.py
@@ -1,9 +1,16 @@
 import socket
 import io
 from threading import Condition
+import threading
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
+import RPi.GPIO as GPIO 
+import subprocess
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -21,13 +28,15 @@ def start_streaming_server():
     picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
     output = StreamingOutput()
     picam2.start_recording(JpegEncoder(), FileOutput(output))
+    shutdown_thread = GPIOShutdownThread()
+    shutdown_thread.start()
+
 
     while True:
         try:
             # Set up a TCP server
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-
             server_socket.bind(('0.0.0.0', 8000))  # Listen on port 8000
             server_socket.listen(1)
             print("Waiting for a connection...")
@@ -57,6 +66,14 @@ def start_streaming_server():
             print(f"An error occurred: {e}")
         finally:
             print("Restarting the server...")
+
+class GPIOShutdownThread(threading.Thread):
+    def run(self):
+        while True:
+            if GPIO.input(17) == GPIO.HIGH:
+                print("Shutdown button pressed. Shutting down the server...")
+                subprocess.run(["sudo", "shutdown", "now"])
+
 
 if __name__ == "__main__":
     start_streaming_server()
